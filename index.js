d9e41a5b6977851e1af7dfe6ad8c865768bc7579
tmhammer <tmhammer92@gmail.com>
2018-04-23 14:58:09 -0400

    Build ES entries

--- index.js
+++ index.js
@@ -5,24 +5,37 @@ const s3 = new AWS.S3();
 const fs = require('fs');
 const _ = require('lodash');
 
+const bucket_name = 'environmental-solutions';
+const freshen_url = 'https://api.trade.gov/v1/environmental_solutions/freshen.json?api_key=';
+const api_key = process.env.API_KEY;
+
 const sf_username = process.env.SF_USERNAME;
 const sf_password = process.env.SF_PASSWORD;
 const conn = new sf.Connection({
 	loginUrl: 'https://trade.my.salesforce.com'
 });
 
-conn.login(sf_username, sf_password, function(err, res){
-	if (err) { return console.error(err); }
-
-	getObjects();
-});
 
 getObjects = function() {
 	// Environmental Solutions: 
-	conn.apex.post('/ToolkitSearch/', {programId: 'a31t0000000CyD6', languageIso: 'en'}, function(err, res){
+	const data = {};
+	conn.apex.post('/ToolkitSearch/', {programId: 'a31t0000000CyD6', languageIso: 'en', }, function(err, res){
 		if (err) { return console.error(err); }
-		processESEntries(res);
+		//writeToFile('environmental_solutions', res);
+		data.api_results = res;
+		conn.query("SELECT ID, Name, Company_Description__c, Website__c FROM Participation__c WHERE Program__c IN ('a31t0000000CyD6')", function(err, res){
+			if (err) { return console.error(err); }
+			//writeToFile('providers', res);
+			data.providers = res;
+			conn.query("SELECT ID, Name, Participant__c FROM Asset WHERE Program__c IN ('a31t0000000CyD6')", function(err, res){
+				if (err) { return console.error(err); }
+				//writeToFile('products_and_providers', res);
+				data.solutions_with_provider = res;
+				processESEntries(data);
+			});
+		});
 	});
+	
 	/*
 	// Civil Nuclear
 	conn.apex.post('/ToolkitSearch/', {programId: 'a31t0000000CyDB'}, function(err, res){
@@ -58,82 +71,156 @@ initializeFields = function(item, fields){
 	}
 }
 
-processESEntries = function(res) {
-	const issues = _.map(res.filterGroups[0].filterItems, function(item, index) {
-		let fields = { issue_id: index, regulation_ids: [], solution_ids: [], type: 'Environmental Issue' };
+processESEntries = function(data) {
+	const issues = _.map(data.api_results.filterGroups[0].filterItems, function(item, index) {
+		let fields = { issue_id: [index], regulation_id: [], solution_id: [], provider_id: [], type: 'Environmental Issue' };
 		initializeFields(item, fields);
 		return item;
 	});
 
-	const regulations = _.map(res.filterGroups[1].filterItems, function(item, index) {
-		let fields = { regulation_id: index, issue_ids: [], solution_ids: [], type: 'EPA Regulation' };
+	const regulations = _.map(data.api_results.filterGroups[1].filterItems, function(item, index) {
+		let fields = { regulation_id: [index], issue_id: [], solution_id: [], provider_id: [], type: 'EPA Regulation' };
 		initializeFields(item, fields);
 		return item;
 	});;
 
-	const solutions = _.map(res.filterGroups[2].filterItems, function(item, index) {
-		let fields = { solution_id: index, regulation_ids: [], issue_ids: [], type: 'Solution'};
+	const solutions = _.map(data.api_results.filterGroups[2].filterItems, function(item, index) {
+		let fields = { solution_id: [index], regulation_id: [], issue_id: [], provider_id: [], type: 'Solution'};
 		initializeFields(item, fields);
 		return item;
 	});;
 
-	for (let key in issues){
-		let issue = issues[key];
+	const providers = _.map(data.providers.records, function(item, index) {
+		let fields = { solution_id: [], regulation_id: [], issue_id: [], provider_id: [index], type: 'Provider'};
+		initializeFields(item, fields);
+		return item;
+	});
 
-		// Find Regulations that match each Issue:
-		let regs = _.filter(regulations, {productIds: issue.productIds});
-		let reg_ids = _.map(regs, 'regulation_id');
+	const solutions_with_provider = buildSolutionsWithProvider(data.solutions_with_provider.records);
 
-		// Find Solutions that are contained by an Issue:
-		let sols = _.filter(solutions, function(item){
-			return issue.productIds.includes(item.productIds[0]);
-		});
-		let sol_ids = _.map(sols, 'solution_id');
+	assignESIds(solutions, issues, regulations, providers, solutions_with_provider)
 
-		// Add Ids for the matching regulations to the Issue:
-		issue.regulation_ids = issue.regulation_ids.concat(reg_ids);
-		// Add solution ids to the issue
-		issue.solution_ids = issue.solution_ids.concat(sol_ids);
+	let all_entries = solutions.concat(issues).concat(regulations).concat(providers)
+	all_entries = processESFields(all_entries)
 
-		// Add Ids for the Issue and Solutions to the matching Regs:
-		regs = _.map(regs, function(reg) {
-			reg.issue_ids.push(issue.issue_id);
-			reg.solution_ids = reg.solution_ids.concat(sol_ids);
-			return reg;
-		});
+	writeToBucket(all_entries)
+}
 
-		// Add Ids for the Issue and Regulations to the matching solutions:
-		sols = _.map(sols, function(sol) {
-			sol.issue_ids.push(issue.issue_id);
-			sol.regulation_ids = sol.regulation_ids.concat(reg_ids);
-			return sol;
-		});
+buildSolutionsWithProvider = function(entries) {
+	const solutions_with_provider = {};
+
+	_.forEach(entries, function(item){
+		if(solutions_with_provider[item.Name]) {
+			solutions_with_provider[item.Name].push(item.Participant__c);
+		}
+		else {
+			solutions_with_provider[item.Name] = [item.Participant__c];
+		}
+	})
+}
+
+
+assignESIds = function(solutions, issues, regulations, providers, solutions_with_provider){
+	for (let key in solutions) {
+		let solution = solutions[key]
+		let iss = _.filter(issues, function(issue){ return issue.productIds.includes(solution.id)})
+		let regs = _.filter(regulations, function(regulation){ return regulation.productIds.includes(solution.id)})
+
+		let reg_ids = _.map(regs, function(reg) { return reg.regulation_id[0] });
+		let issue_id = _.map(iss, function(issue) { return issue.issue_id[0] });
+
+		let provider_raw_ids = _.compact(solutions_with_provider[solution.name]);
+		let provs = _.filter(providers, function(provider) { return provider_raw_ids.includes(provider.Id) })
+		let provider_id = _.map(provs, function(provider) { return provider.provider_id[0] });
+
+		//Add all ids to solution:
+		solution.regulation_id = solution.regulation_id.concat(reg_ids)
+		solution.issue_id = solution.issue_id.concat(issue_id)
+		solution.provider_id = solution.provider_id.concat(provider_id)
+
+		//Add all ids to each issue:
+		iss = _.map(iss, function(issue){
+			issue.solution_id = issue.solution_id.concat(solution.solution_id)
+			issue.regulation_id = _.uniq(issue.regulation_id.concat(reg_ids))
+			issue.provider_id = _.uniq(issue.provider_id.concat(provider_id))
+			return issue
+		})
+
+		//Add all ids to each regulation:
+		regs = _.map(regs, function(reg){
+			reg.solution_id = reg.solution_id.concat(solution.solution_id)
+			reg.issue_id = _.uniq(reg.issue_id.concat(issue_id))
+			reg.provider_id = _.uniq(reg.provider_id.concat(provider_id))
+			return reg
+		})
+
+		//Add all ids to each Provider:
+		provs = _.map(provs, function(prov){
+			prov.solution_id = prov.solution_id.concat(solution.solution_id)
+			prov.issue_id = _.uniq(prov.issue_id.concat(issue_id))
+			prov.regulation_id = _.uniq(prov.regulation_id.concat(reg_ids))
+			return prov
+		})
 
 	}
-	//console.log(JSON.stringify(issues, null, 4))
-	//console.log(JSON.stringify(regulations, null, 4))
-	//console.log(JSON.stringify(solutions, null, 4))
 }
 
-processCNEntries = function(res) {
-
+processESFields = function(all_entries) {
+	let new_entries = _.map(all_entries, function(entry) {
+		entry = _.omit(entry, ['productIds', 'linksSize', 'id', 'attributes', 'Id'])
+		if(entry.type === 'Provider'){
+			entry.summary = entry.Company_Description__c;
+			entry.name = entry.Name;
+			entry.links = [{url: entry.Website__c, display_name: entry.name}]
+			delete entry.Company_Description__c
+			delete entry.Name
+			delete entry.Website__c
+		} else if(!_.isEmpty(entry.links)){
+			entry.links = _.map(entry.links, function(link){ 
+				link.display_name = link.name
+				delete link.id
+				delete link.name
+				return link
+			})
+		}
+		if(!entry.summary)
+			entry.summary = null
+		return entry
+	})
+	return new_entries
 }
 
-processNGEntries = function(res) {
-
+writeToBucket = function(entries) {
+	const params = {
+		Body: JSON.stringify(entries, null, 2),
+		Bucket: bucket_name,
+		Key: 'results.json',
+		ACL: 'public-read',
+		ContentType: 'application/json'
+	};
+	s3.putObject(params, function(err, data){
+		if (err) { return console.error(err); }
+		console.log('File successfully uploaded!');
+		freshenEndpoint();
+	});
 }
 
-processSGEntries = function(res){
-
+freshenEndpoint = function() {
+	request(freshen_url+api_key, function(err, res, body) {
+		if (err || (res && res.statusCode!= '200')) { return console.error('An error occurred while freshening the endpoint.'); }
+		console.log(res.statusCode)
+		console.log('Endpoint successfully updated!')
+	});
 }
 
-processOGEntries = function(res){
-	
-}
 
-processREEntries = function(res){
-	
-}
+exports.handler = function(event, context, callback) {
+  conn.login(sf_username, sf_password, function(err, res){
+		if (err) { return console.error(err); }
+
+		getObjects();
+	});
+};
 
 writeToFile = function(name, data) {
 	fs.writeFile(name + ".json", JSON.stringify(data, null, 4), function(err) {
@@ -144,15 +231,3 @@ writeToFile = function(name, data) {
     console.log("The " + name + " file was saved!");
 	}); 
 }
-
-// For development/testing purposes
-exports.handler = function(event, context, callback) {
-  console.log('Running index.handler');
-  console.log('==================================');
-  console.log('event', event);
-  console.log('==================================');
-  console.log('Stopping index.handler');
-  callback(null, event);
-  // or
-  // callback( 'some error type' );
-};
